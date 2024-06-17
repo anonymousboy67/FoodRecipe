a1cf597e75127362967d9823d345cc666c332a5d
anonymousboy67 <aashishad67@gmail.com>
2024-06-17 06:54:00 +0545

    Change for deployment

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="dark:bg-white text-lightBlack font-extrabold text-center p-4 ">
+      <h1 className="dark:bg-white text-lightBlack font-extrabold marker:text-center p-4 ">
         Welcome to Recipe App
       </h1>
       <Link href={"/recipe-list"}>Explore Recipe</Link>
